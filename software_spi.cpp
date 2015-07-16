61124be25292e7edf34e64d829b787d94ebe3796
astroeng <dschacht@gmail.com>
2015-07-15 23:09:17 -0400

    Removed on of the delays so that the clock signal is a bit more traditional with a 50% duty cycle. Updated the read part of the library to account for the possibility of MSB first or LSB first.

--- software_spi.cpp
+++ software_spi.cpp
@@ -3,6 +3,10 @@
 
 
 #define DELAY_CYCLE() delayMicroseconds(_clock_uS)
+#define CLOCK_HIGH() digitalWrite(_clock_pin, 1)
+#define CLOCK_LOW() digitalWrite(_clock_pin, 0)
+#define WRITE_BIT(x) digitalWrite(_output_pin, x)
+#define READ_BIT() digitalRead(_input_pin)
 
 Software_SPI::Software_SPI(int input_pin,
                            int output_pin,
@@ -23,33 +27,81 @@ Software_SPI::Software_SPI(int input_pin,
 
 }
 
+void Software_SPI::reset()
+{
+  CLOCK_LOW();
+  WRITE_BIT(0);
+  
+}
+
 unsigned char Software_SPI::SPI_Send_Receive(unsigned char output)
 {
   unsigned char input = 0;
+  unsigned char temp;
   char bit_loop;
-
+  
+  CLOCK_LOW();
+  
   for (bit_loop = 0; bit_loop < 8; bit_loop++)
   {
-    digitalWrite(_clock_pin, 0);
-    
-    DELAY_CYCLE();
+
+    /* Drive the output value for the slave device. Then wait the low
+       half of the clock cycle. 
+     */
     
     if (_bit_order == MSB_FIRST)
     {
-      digitalWrite(_output_pin, ((output & 0x80) > 0 ? 1 : 0));
+      temp = ((output & 0x80) > 0 ? 1 : 0);
+      
+      WRITE_BIT(temp);
+      
       output = output << 1;
     }
     else 
     {
-      digitalWrite(_output_pin, ((output & 0x01) > 0 ? 1 : 0));
+      temp = ((output & 0x01) > 0 ? 1 : 0);
+      
+      WRITE_BIT(temp);
+      
       output = output >> 1;
     }
     
-    digitalWrite(_clock_uS, 1);
+    DELAY_CYCLE();
+    
+    /* Set the clock pin high; this will trigger the output bit to be read
+       by the slave device. It will also cause the slave device to drive
+       the input bit. Then wait the high half of the clock signal.
+     */
+    
+    CLOCK_HIGH();
     
     DELAY_CYCLE();
     
-    input = input << 1;
-    input |= digitalRead(_input_pin) > 0 ? 1 : 0;
+    /* Now that the data is stable.... read it. */
+    
+    if (_bit_order == MSB_FIRST)
+    {
+      input = input << 1;
+      
+      temp = READ_BIT() > 0 ? 0x01 : 0x00;
+      
+      input |= temp;
+      
+    }
+    else 
+    {
+      input = input >> 1;
+      
+      temp = READ_BIT() > 0 ? 0x80 : 0x00;
+      
+      input |= temp;
+    }
+    
+    CLOCK_LOW();
+    
   }
+  
+  
+  return input;
+
 }
--- software_spi.h
+++ software_spi.h
@@ -5,6 +5,19 @@
 #define MSB_FIRST 12
 #define LSB_FIRST 13
 
+/* This class will, given the correct pins, handle the transactions on an 
+   SPI bus. This class does not use the chip select pin and that should be
+   handled by outside code. Doing so allows a single SPI class to interface
+   with many SPI devices.
+ 
+   The SPI_Send_Receive function input will be written to the SPI bus. At 
+   the same time the function will read the input pin and return the resultant
+   byte as the return parameter. This enables the most efficient use of the 
+   bus as data can move in both directions at the same time. If a device does
+   not support such a transaction then 0x00 may be written to the SPI bus
+   during read operations.
+ */
+
 class Software_SPI
 {
 public:
@@ -15,6 +28,7 @@ public:
                int clock_uS);
 
   unsigned char SPI_Send_Receive(unsigned char output);
+  void reset();
   
 private:
 
